bb474a218ec428663060c0a8eb5da0bd0ec7178d
Harun29 <ibrahimovic.harun01@gmail.com>
2024-03-12 11:29:16 +0100

    finished restaurant rating form section

--- src/components/admin/AddRestaurant.jsx
+++ src/components/admin/AddRestaurant.jsx
@@ -23,17 +23,17 @@ const AddRestaurant = () => {
 
   const [food, setFood] = useState([0]);
   const [foodForUpload, setFoodForUpload] = useState([{}]);
-  const [overallFoodRating, setOverallFoodRating] = useState()
+  const [overallFoodRating, setOverallFoodRating] = useState();
 
-  const [exteriorRating, setExteriorRating] = useState()
-  const [interiorRating, setInteriorRating] = useState()
-  const [locationRating, setLocationRating] = useState()
-  const [overallLooksRating, setOverallLooksRating] = useState()
-  
-  const [hostingRating, setHostingRating] = useState()
-  const [foodPresentationRating, setFoodPresentationRating] = useState()
-  const [customerServiceRating, setCustomerServiceRating] = useState()
-  const [overallServiceRating, setOverallServiceRating] = useState()
+  const [exteriorRating, setExteriorRating] = useState(0);
+  const [interiorRating, setInteriorRating] = useState(0);
+  const [locationRating, setLocationRating] = useState(0);
+  const [overallLooksRating, setOverallLooksRating] = useState();
+
+  const [hostingRating, setHostingRating] = useState(0);
+  const [foodPresentationRating, setFoodPresentationRating] = useState(0);
+  const [customerServiceRating, setCustomerServiceRating] = useState(0);
+  const [overallServiceRating, setOverallServiceRating] = useState();
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -96,7 +96,7 @@ const AddRestaurant = () => {
     const object = {
       description: "",
       selectedImage: "",
-      id: v4()
+      id: v4(),
     };
     setFood((prevFoodCounter) => [...prevFoodCounter, prevFoodCounter.length]);
     setFoodForUpload((prevFoodCounter) => [...prevFoodCounter, object]);
@@ -131,20 +131,36 @@ const AddRestaurant = () => {
     }
     setFoodForUpload(newFood);
   };
-  
+
   const handleDeleteFood = (index) => {
-    const newFood = [...foodForUpload]
-    const newFoodCounter = [...food]
-    newFood.splice(index, 1)
-    newFoodCounter.pop()
+    const newFood = [...foodForUpload];
+    const newFoodCounter = [...food];
+    newFood.splice(index, 1);
+    newFoodCounter.pop();
     setFood(newFoodCounter);
     setFoodForUpload(newFood);
-  }
+  };
 
   useEffect(() => {
-    console.log(food)
-    console.log(foodForUpload)
-  }, [food, foodForUpload])
+    console.log(food);
+    console.log(foodForUpload);
+  }, [food, foodForUpload]);
+
+  const handleRestaurantRating = (type, rating) => {
+    if (type === "exterior") {
+      setExteriorRating(rating);
+    } else if (type === "interior") {
+      setInteriorRating(rating);
+    } else if (type === "location") {
+      setLocationRating(rating);
+    } else if (type === "host") {
+      setHostingRating(rating);
+    } else if (type === "presentation") {
+      setFoodPresentationRating(rating);
+    } else if (type === "service") {
+      setCustomerServiceRating(rating);
+    }
+  };
 
   return !addFood ? (
     <form
@@ -357,35 +373,40 @@ const AddRestaurant = () => {
               </div>
             </div>
           </div>
-          {index > 0 && <button onClick={() => handleDeleteFood(index)} class="deleteButton">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 50 59"
-              class="bin"
+          {index > 0 && (
+            <button
+              onClick={() => handleDeleteFood(index)}
+              class="deleteButton"
             >
-              <path
-                fill="#B5BAC1"
-                d="M0 7.5C0 5.01472 2.01472 3 4.5 3H45.5C47.9853 3 50 5.01472 50 7.5V7.5C50 8.32843 49.3284 9 48.5 9H1.5C0.671571 9 0 8.32843 0 7.5V7.5Z"
-              ></path>
-              <path
-                fill="#B5BAC1"
-                d="M17 3C17 1.34315 18.3431 0 20 0H29.3125C30.9694 0 32.3125 1.34315 32.3125 3V3H17V3Z"
-              ></path>
-              <path
-                fill="#B5BAC1"
-                d="M2.18565 18.0974C2.08466 15.821 3.903 13.9202 6.18172 13.9202H43.8189C46.0976 13.9202 47.916 15.821 47.815 18.0975L46.1699 55.1775C46.0751 57.3155 44.314 59.0002 42.1739 59.0002H7.8268C5.68661 59.0002 3.92559 57.3155 3.83073 55.1775L2.18565 18.0974ZM18.0003 49.5402C16.6196 49.5402 15.5003 48.4209 15.5003 47.0402V24.9602C15.5003 23.5795 16.6196 22.4602 18.0003 22.4602C19.381 22.4602 20.5003 23.5795 20.5003 24.9602V47.0402C20.5003 48.4209 19.381 49.5402 18.0003 49.5402ZM29.5003 47.0402C29.5003 48.4209 30.6196 49.5402 32.0003 49.5402C33.381 49.5402 34.5003 48.4209 34.5003 47.0402V24.9602C34.5003 23.5795 33.381 22.4602 32.0003 22.4602C30.6196 22.4602 29.5003 23.5795 29.5003 24.9602V47.0402Z"
-                clip-rule="evenodd"
-                fill-rule="evenodd"
-              ></path>
-              <path
-                fill="#B5BAC1"
-                d="M2 13H48L47.6742 21.28H2.32031L2 13Z"
-              ></path>
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 50 59"
+                class="bin"
+              >
+                <path
+                  fill="#B5BAC1"
+                  d="M0 7.5C0 5.01472 2.01472 3 4.5 3H45.5C47.9853 3 50 5.01472 50 7.5V7.5C50 8.32843 49.3284 9 48.5 9H1.5C0.671571 9 0 8.32843 0 7.5V7.5Z"
+                ></path>
+                <path
+                  fill="#B5BAC1"
+                  d="M17 3C17 1.34315 18.3431 0 20 0H29.3125C30.9694 0 32.3125 1.34315 32.3125 3V3H17V3Z"
+                ></path>
+                <path
+                  fill="#B5BAC1"
+                  d="M2.18565 18.0974C2.08466 15.821 3.903 13.9202 6.18172 13.9202H43.8189C46.0976 13.9202 47.916 15.821 47.815 18.0975L46.1699 55.1775C46.0751 57.3155 44.314 59.0002 42.1739 59.0002H7.8268C5.68661 59.0002 3.92559 57.3155 3.83073 55.1775L2.18565 18.0974ZM18.0003 49.5402C16.6196 49.5402 15.5003 48.4209 15.5003 47.0402V24.9602C15.5003 23.5795 16.6196 22.4602 18.0003 22.4602C19.381 22.4602 20.5003 23.5795 20.5003 24.9602V47.0402C20.5003 48.4209 19.381 49.5402 18.0003 49.5402ZM29.5003 47.0402C29.5003 48.4209 30.6196 49.5402 32.0003 49.5402C33.381 49.5402 34.5003 48.4209 34.5003 47.0402V24.9602C34.5003 23.5795 33.381 22.4602 32.0003 22.4602C30.6196 22.4602 29.5003 23.5795 29.5003 24.9602V47.0402Z"
+                  clip-rule="evenodd"
+                  fill-rule="evenodd"
+                ></path>
+                <path
+                  fill="#B5BAC1"
+                  d="M2 13H48L47.6742 21.28H2.32031L2 13Z"
+                ></path>
+              </svg>
 
-            <span class="tooltip">Delete</span>
-          </button>}
+              <span class="tooltip">Delete</span>
+            </button>
+          )}
         </div>
       ))}
       <div className="add-more-food">
@@ -410,6 +431,184 @@ const AddRestaurant = () => {
           </span>
         </button>
       </div>
+      <span className="add-food-header">Ocijeni restoran:</span>
+      <div className="restaurant-rating-container">
+        <div className="restaurant-looks">
+          <span>Eksterijer</span>
+          <div>
+            <Star
+              fill={exteriorRating >= 1 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("exterior", 1)}
+            ></Star>
+            <Star
+              fill={exteriorRating >= 2 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("exterior", 2)}
+            ></Star>
+            <Star
+              fill={exteriorRating >= 3 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("exterior", 3)}
+            ></Star>
+            <Star
+              fill={exteriorRating >= 4 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("exterior", 4)}
+            ></Star>
+            <Star
+              fill={exteriorRating >= 5 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("exterior", 5)}
+            ></Star>
+          </div>
+          <span>Enterijer</span>
+          <div>
+            <Star
+              fill={interiorRating >= 1 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("interior", 1)}
+            ></Star>
+            <Star
+              fill={interiorRating >= 2 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("interior", 2)}
+            ></Star>
+            <Star
+              fill={interiorRating >= 3 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("interior", 3)}
+            ></Star>
+            <Star
+              fill={interiorRating >= 4 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("interior", 4)}
+            ></Star>
+            <Star
+              fill={interiorRating >= 5 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("interior", 5)}
+            ></Star>
+          </div>
+          <span>Lokacija</span>
+          <div>
+            <Star
+              fill={locationRating >= 1 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("location", 1)}
+            ></Star>
+            <Star
+              fill={locationRating >= 2 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("location", 2)}
+            ></Star>
+            <Star
+              fill={locationRating >= 3 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("location", 3)}
+            ></Star>
+            <Star
+              fill={locationRating >= 4 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("location", 4)}
+            ></Star>
+            <Star
+              fill={locationRating >= 5 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("location", 5)}
+            ></Star>
+          </div>
+        </div>
+        <div className="restaurant-service">
+          <span>Docek gosta</span>
+          <div>
+            <Star
+              fill={hostingRating >= 1 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("host", 1)}
+            ></Star>
+            <Star
+              fill={hostingRating >= 2 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("host", 2)}
+            ></Star>
+            <Star
+              fill={hostingRating >= 3 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("host", 3)}
+            ></Star>
+            <Star
+              fill={hostingRating >= 4 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("host", 4)}
+            ></Star>
+            <Star
+              fill={hostingRating >= 5 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("host", 5)}
+            ></Star>
+          </div>
+          <span>Prezentacija hrane</span>
+          <div>
+            <Star
+              fill={foodPresentationRating >= 1 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("presentation", 1)}
+            ></Star>
+            <Star
+              fill={foodPresentationRating >= 2 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("presentation", 2)}
+            ></Star>
+            <Star
+              fill={foodPresentationRating >= 3 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("presentation", 3)}
+            ></Star>
+            <Star
+              fill={foodPresentationRating >= 4 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("presentation", 4)}
+            ></Star>
+            <Star
+              fill={foodPresentationRating >= 5 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("presentation", 5)}
+            ></Star>
+          </div>
+          <span>Odnos prema gostima</span>
+          <div>
+            <Star
+              fill={customerServiceRating >= 1 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("service", 1)}
+            ></Star>
+            <Star
+              fill={customerServiceRating >= 2 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("service", 2)}
+            ></Star>
+            <Star
+              fill={customerServiceRating >= 3 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("service", 3)}
+            ></Star>
+            <Star
+              fill={customerServiceRating >= 4 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("service", 4)}
+            ></Star>
+            <Star
+              fill={customerServiceRating >= 5 ? "gold" : "none"}
+              size={"40"}
+              onClick={() => handleRestaurantRating("service", 5)}
+            ></Star>
+          </div>
+        </div>
+      </div>
+      <button disabled={loading} className="recepie-submit finish">
+        Finish
+      </button>
     </div>
   );
 };
